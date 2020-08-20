8ea837f525d03627f18eea81424906f7637f11b7
taajuddin <taajraza@gmail.com>
2020-08-20 21:38:47 +0530

    Updates

---
+++ precache-manifest.f1162d2ac2c8432becc4cfab4ed069e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3838a8e4be2bf9e98ba0bc093b4ba36",
+    "url": "/job-application-form/index.html"
+  },
+  {
+    "revision": "57dd0f6abda23f1db5df",
+    "url": "/job-application-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ab7f1864e6477adbf647",
+    "url": "/job-application-form/static/js/2.7395c499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-application-form/static/js/2.7395c499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57dd0f6abda23f1db5df",
+    "url": "/job-application-form/static/js/main.91a2cd3e.chunk.js"
+  },
+  {
+    "revision": "1039ea67484770b663e2",
+    "url": "/job-application-form/static/js/runtime-main.f322ceb2.js"
+  }
+]);
